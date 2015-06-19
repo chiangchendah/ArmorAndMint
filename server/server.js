1fd51e1bd1fee10d4b5d64f046a08c62ae6e02c1
Michael Symmes <msymmes@gmail.com>
2015-06-19 15:00:12 -0400

    Wired up the database connection, moved express config/startup into its own file. Added more skeleton to the project.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,28 @@
 // config/requirements
-var express = require('express');
-var app = express();
-var path = require('path');
-var port = process.env.PORT || 8080;
+var mongoose = require('mongoose');
 
-// serve assets from our client directory
-app.use(express.static(path.join(__dirname, '../client')));
+// we are delegating our express configuration and setup to this file
+var express = require('./config/express');
 
-// start the server
-app.listen(port);
-console.log('Listening on port ' + port);
+// app specific configuration can be done in this file
+var config = require('./config/config');
+
+// create a db connection
+var db = mongoose.connection;
+
+// create a connection string -> 'mongodb://localhost/test'
+var dbConnectionString = 'mongodb://' + config.db.url + '/' + config.db.name;
+
+// create the connection
+mongoose.connect(dbConnectionString);
+
+// Database Error handling
+db.on('error', console.error.bind(console, 'connection error:'));
+
+// Run our entire app inside the database connection callback
+db.once('open', function (callback) {
+  // call our express configuration/setup here
+  var app = express();
+});
+
+
