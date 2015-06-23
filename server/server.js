f5e65ab039ea3f1651b9ed4f9a6d09bd5ab9543e
Michael Symmes <msymmes@gmail.com>
2015-06-23 15:29:50 -0400

    Formatting

--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ var config = require('./config/config'); // app specific configuration can be do
 // Modular variables
 var db = mongoose.connection; // create a db connection
 
-
 var dbConnectionString = 'mongodb://' + config.db.url + ':' + config.db.port + '/' + config.db.name; // create a connection string -> 'mongodb://localhost:port/test'
 mongoose.connect(dbConnectionString); // create the connection
 db.on('error', console.error.bind(console, 'connection error:')); // Database Error handling
