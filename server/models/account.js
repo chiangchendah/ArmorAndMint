e1b9c96d0dd3b37e29e1c94846e69e2daca9dee7
Michael Symmes <msymmes@gmail.com>
2015-06-23 14:30:20 -0400

    Changed this to a more standard mongoose schema verbage

--- server/models/account.js
+++ server/models/account.js
@@ -2,11 +2,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
-var Account = new Schema({
+var UserSchema = new Schema({
     username: String,
     password: String
 });
 
-Account.plugin(passportLocalMongoose);
+UserSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model('Account', Account);
+module.exports = mongoose.model('User', UserSchema);
