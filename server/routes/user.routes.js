f2032a28818281bd00462dd401e2cf2404279109
Michael Symmes <msymmes@gmail.com>
2015-06-24 17:40:27 -0400

    Requiring passport from this file directly instead of passing it in

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,6 +1,7 @@
 var User = require('../controllers/user.controller.js');
+var passport = require('passport');
 
-module.exports = function(app, passport) {
+module.exports = function(app) {
   // http://mherman.org/blog/2015/01/31/local-authentication-with-passport-and-express-4/#.VYiH3hNVhHw
 
   app.post('/register', function(req, res, next) {
