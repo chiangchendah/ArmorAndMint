e4ac171f9775a1439184e1d91ef2d151df5b859c
Michael Symmes <msymmes@gmail.com>
2015-06-26 19:52:39 -0400

    Added comments/todo

--- client/app/article/article.js
+++ client/app/article/article.js
@@ -1,14 +1,21 @@
-// This is the main file for our ArticleController
-// it should handle all requests that the Article(s) views might need
+// ArticleController
+// handles all requests that the Article(s) views/templates might need
 
 angular.module('lightCMS.article', [])
     .controller('ArticleController', function($scope, Articles){
 
-      // mocks and stubs - delete at will
-      $scope.data = {};
+      // this will need some $scope methods for interfacing from the template
 
-      // this will need some $scope methods for calling
-      // the Articles service, which will handle the http requests
-      Articles.fetchAll();
-      Articles.fetchOne('558b3aecb8506f2f03856194');
+      // example:
+      // $scope.listArticles = function(){
+      //   $scope.articles = Articles.fetchAll()
+      // };
+
+      // this is where we need to trigger edit events?
+
+      // use the Articles service for api requests
+        // Articles.fetchAll(); -- returns all articles
+        // Articles.fetchOne('558b3aecb8506f2f03856194'); -- takes an article id, returns an article
+        // Articles.create({title: 'A day in the life.', body: 'Yo ho, a pirates life for me!'});
+          // ^ takes an object with at least title and body - creates new article
     });
