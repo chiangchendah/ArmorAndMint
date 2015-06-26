82a495c93c5739970c254bf0cec195df65847f1b
Michael Symmes <msymmes@gmail.com>
2015-06-26 19:46:19 -0400

    Added comments

--- client/app/services.js
+++ client/app/services.js
@@ -2,22 +2,38 @@
 //
 // Handles all crud requests for our article element
 // server should conform to RESTful routes
+//
+// This is implemented barebones, and could use much refinement.
 angular.module('lightCMS.ArticleService', [])
   .factory('Articles', function($http) {
     var article = {};
 
+    // How much should we try to hold on/memoize to data here?
+    // Im not sure if we should just always hit the api or what?
+
+    // fetchAll()
+    // returns an array of all articles
+    // todo:
+    //    allow limits in the # returned?
+    //    (angular) filters?
+    //    better error handling
     article.fetchAll = function(){
       $http.get('/articles')
         .success(function(data, status, headers, config){
           console.log('Recieved: ', data, status, headers, config);
+          // TODO: return a promise here?
           return data;
         })
         .error(function(data, status, headers, config){
           console.error(data, status, headers, config);
         });
       };
+
+    // fetchOne()
+    // takes an article id as a string
+    // returns one article or (null? err? what?)
     article.fetchOne = function(id){
-      console.log('Fetching');
+      console.log('Fetching one');
       $http.get('/articles/' + id)
         .success(function(data, status, headers, config){
           console.log('Got one: ',  data, status, headers, config);
@@ -27,9 +43,13 @@ angular.module('lightCMS.ArticleService', [])
           console.error(data, status, headers, config);
         });
     };
+
+    // create()
+    // takes an article object
+    // it should have at least a title and a body property
     article.create = function(article){
-      // do some validation here?
-      $http.post('/articles', data)
+      // TODO: do some validation here?
+      $http.post('/articles', article)
         .success(function(data) {
           console.log('Successfully posted new article;');
         })
@@ -37,6 +57,6 @@ angular.module('lightCMS.ArticleService', [])
           console.log('Error: ', data);
         });
     };
-    // TODO: update(.put) and delete
+    // TODO: .put(a.k.a. delete) and delete
     return article;
 });
