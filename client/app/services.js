bd5e9f392c81a6cc0441ba05eb86e3ccdb53f755
Michael Symmes <msymmes@gmail.com>
2015-06-27 17:26:46 -0400

    Added a stub userauth service and started changing some articles service methods to return promises

--- client/app/services.js
+++ client/app/services.js
@@ -5,6 +5,17 @@
 //
 // This is implemented barebones, and could use much refinement.
 angular.module('lightCMS.ArticleService', [])
+  .factory('User', function($http){
+    var user = {};
+
+    // stubbed for now - in the future this should actually do stuff
+    // like you know.. check if the user is actually authed
+    user.isAuthed = function() {
+      return true;
+    };
+
+    return user;
+  })
   .factory('Articles', function($http) {
     var article = {};
 
@@ -12,36 +23,23 @@ angular.module('lightCMS.ArticleService', [])
     // Im not sure if we should just always hit the api or what?
 
     // fetchAll()
-    // returns an array of all articles
+    // returns a promise
+    // that in turn tries to return all articles
     // todo:
     //    allow limits in the # returned?
     //    (angular) filters?
     //    better error handling
     article.fetchAll = function(){
-      $http.get('/articles')
-        .success(function(data, status, headers, config){
-          console.log('Recieved: ', data, status, headers, config);
-          // TODO: return a promise here?
-          return data;
-        })
-        .error(function(data, status, headers, config){
-          console.error(data, status, headers, config);
-        });
-      };
+      return $http.get('/articles');
+    };
 
     // fetchOne()
     // takes an article id as a string
-    // returns one article or (null? err? what?)
+    // returns a promise that will try to
+    // provide one article or (null? err? what?)
     article.fetchOne = function(id){
       console.log('Fetching one');
-      $http.get('/articles/' + id)
-        .success(function(data, status, headers, config){
-          console.log('Got one: ',  data, status, headers, config);
-          return data;
-        })
-        .error(function(data, status, headers, config){
-          console.error(data, status, headers, config);
-        });
+      return $http.get('/articles/' + id);
     };
 
     // create()
