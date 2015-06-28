81f8aa84bd17f24bae4b6aff4505a756d0ee3884
Michael Symmes <msymmes@gmail.com>
2015-06-28 05:46:49 -0400

    cleaned up console.logs and added some comments

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,15 +1,18 @@
+// user.routes.js
+//
+// passes control to the user controller
+// for given routes (signin, signout, register)
+//
 var User = require('../controllers/user.controller.js');
 var passport = require('passport');
 
 module.exports = function(app) {
-  // http://mherman.org/blog/2015/01/31/local-authentication-with-passport-and-express-4/#.VYiH3hNVhHw
 
   app.post('/register', function(req, res, next) {
     User.register(req, res, next);
   });
 
   app.post('/signin', passport.authenticate('local'), function(req, res, next) {
-    console.log('authed!');
     User.signin(req, res, next);
   });
 
