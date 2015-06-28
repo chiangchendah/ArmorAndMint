bd305414c564723fd8973043a94806fb3307c608
Michael Symmes <msymmes@gmail.com>
2015-06-28 19:52:52 -0400

    Added unique title requirement

--- server/models/article.model.js
+++ server/models/article.model.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ArticleSchema = new Schema({
-  title: {type: String, required: true},
+  title: {type: String, required: true, unique: true},
   body: {type: String, required: true},
   createdOn: {type: Date, default: Date.now, required: true},
   author: {type: String, required: true}
