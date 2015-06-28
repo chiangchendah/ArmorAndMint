130939f6629c2e718499a83eeeca57117ada96b4
Michael Symmes <msymmes@gmail.com>
2015-06-28 00:51:26 -0400

    Finalizing rollout to crud view/route changes

--- client/app/article/article.js
+++ client/app/article/article.js
@@ -2,7 +2,9 @@
 // handles all requests that the Article(s) views/templates might need
 
 angular.module('lightCMS.article', [])
-    .controller('ArticlesController', function($scope, Articles, User, $state, $stateParams){
+
+    // interact with a list of articles
+    .controller('ArticlesController', function($scope, Articles, User){
 
       // track our user so we can determine if he has edit rights or not
       $scope.user = User;
@@ -19,55 +21,52 @@ angular.module('lightCMS.article', [])
             console.error(data, status, headers, config);
           });
     })
-    .controller('ArticleController', function($scope, Articles, User, $state, $stateParams){
-      $scope.user = User;
-      $scope.currentArticle = {};
-      Articles.fetchOne($stateParams.id)
-          .then(
-            function(data){
-              $scope.currentArticle = data.data;
-            },
-            function(data, status, headers, config){
-              console.error(data, status, headers, config);
-            });
-    })
-    .controller('EditArticleController', function($scope, Articles, User, $state, $stateParams){
-      $scope.currentArticle = {};
-      Articles.fetchOne($stateParams.id)
-          .then(
-            function(data){
-              $scope.currentArticle = data.data;
-              console.log(data.data);
-            },
-            function(data, status, headers, config){
-              console.error(data, status, headers, config);
-            });
-    })
-    .controller('CreateArticleController', function($scope, Articles, User, $state, $stateParams){
+    // This may indeed be an abuse of controllers
+    // the reason I ended up putting each 'view' in its own controller
+    // was because it made sense with the routing at the time.
+    // that said... prepare for.. wall of controllers:
 
+    // view one article $stateParams.id is set by ui-router
+    .controller('ArticleController', function($scope, Articles, User, $stateParams){
       $scope.user = User;
+      $scope.currentArticle = {};
+      Articles.fetchOne($stateParams.id)
+        .then(
+          function(data){
+            $scope.currentArticle = data.data;
+          },
+          function(data, status, headers, config){
+            console.error(data, status, headers, config);
+          });
+    })
+
+    // edit an article
+    .controller('EditArticleController', function($scope, Articles, $stateParams){
+      $scope.currentArticle = {};
+      Articles.fetchOne($stateParams.id)
+        .then(
+          function(data){
+            $scope.currentArticle = data.data;
+          },
+          function(data, status, headers, config){
+            console.error(data, status, headers, config);
+          });
+    })
+
+    //  create a new article
+    .controller('CreateArticleController', function($scope, Articles, $location){
       $scope.article = {};
-
-      // create new
       $scope.create = function() {
-        console.log('Gotcha');
+        Articles.create($scope.article)
+          .then(
+            function(data){
+              // redirect since we know we are good?
+              $location.url('articles');
+            },
+            function(err){
+              // alert the user to why we couldnt create the article
+              console.error(err);
+            }
+          );
       };
-
-      // If we are in our articles view and we have an id on our $stateParams
-      // if ($scope.view === 'view'){
-      //   console.log('state changed:', $stateParams.id);
-      //   if ($stateParams.id){
-      //     $scope.fetch($stateParams.id);
-      //   }
-      // } else if ($scope.view === 'articles.edit') {
-      //   if ($stateParams.id){
-      //     $scope.editArticle($stateParams.id);
-      //   }
-      // }
-      // use the Articles service for api requests
-
-        // Articles.fetchAll(); -- returns all articles
-        // Articles.fetchOne('558b3aecb8506f2f03856194'); -- takes an article id, returns an article
-        // Articles.create({title: 'A day in the life.', body: 'Yo ho, a pirates life for me!'});
-          // ^ takes an object with at least title and body - creates new article
     });
