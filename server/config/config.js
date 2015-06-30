b54102b9baf79bd08d6e398362b46ca6f24dde92
Sean Grogg <sean.grogg@gmail.com>
2015-06-29 18:27:44 -0700

    hotfix to config.js to remove unnecessary db object

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,6 @@
 // this file contains 'global' configuration info
+// Do we really need this that much anymore with the db portion gone? Can it not be reintegrated into express.js?
 module.exports = {
   port: process.env.PORT || 8080, // the server port
   secret: 'poptart eating cats like lasagna too',
-  // TODO: stop putting this on github/ use a config.example.js?
-  db: {
-    // This object is used to create the database connection string
-    // THIS MAY BE EXTRANEOUS NOW
-    url: process.env.MONGOLAB_URL || '127.0.0.1',
-    port: process.env.MONGOLAB_PORT || 27017,
-    name: process.env.MONGOLAB_DB || 'lightCMS'
-  },
 };
