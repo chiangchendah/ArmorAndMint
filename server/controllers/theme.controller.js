91d233cae820139cca9e74902b9ded8627a14f3e
bryan <bry_angelo@yahoo.com>
2015-07-02 20:11:43 -0700

    directory changes, css changes, almost complete api

--- server/controllers/theme.controller.js
+++ server/controllers/theme.controller.js
@@ -2,7 +2,12 @@ var Theme = require('../config/themes.json');
 
 module.exports = {
   returnThemes: function(req, res, next) {
+<<<<<<< HEAD
     res.send(Theme);
     // res.send([]);
+=======
+    // res.send(Theme);
+    res.send('Hello World');
+>>>>>>> directory changes, css changes, almost complete api
   };
 }
